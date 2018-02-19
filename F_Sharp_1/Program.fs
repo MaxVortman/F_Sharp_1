a977344c97e467b71bdca0b90f10c974102b455a
MaxVortman <maka.abort@gmail.com>
2018-02-19 17:46:11 +0300

    Solved reverse problem.

--- F_Sharp_1/Program.fs
+++ F_Sharp_1/Program.fs
@@ -18,9 +18,12 @@ let fibonacci x : bigint =
         | _ -> fibSeq |> Seq.take (x - 1) |> Seq.last
 
 
+let reverseList list = [for i in (List.length list) - 1  .. -1 .. 0 -> list.[i]]
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Choose the problem:\n1-factorial\n2-fibonacci\n"
+    printfn "Choose the problem:\n1-factorial\n2-fibonacci\n3-reverse\n"
     let problemNumber = Convert.ToInt32(Console.ReadLine())
     if problemNumber = 1 then
         printfn "Enter the number"
@@ -30,5 +33,11 @@ let main argv =
         printfn "Enter the number"
         let x = Convert.ToInt32(Console.ReadLine())
         printfn "Fibonacci : %A" (fibonacci x)
+    elif problemNumber = 3 then
+        printfn "Enter the list elem"
+        let list = Console.ReadLine().Split() |> List.ofArray
+        printfn "Reversed list : "
+        printfn "%A " (reverseList list)
+
     Console.ReadKey() |> ignore
     0 // return an integer exit code
