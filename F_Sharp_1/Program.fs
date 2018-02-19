a127b4ad43d7c2762ce1e76f29b8510bc3440973
MaxVortman <maka.abort@gmail.com>
2018-02-19 13:47:56 +0300

    Добавьте файлы проекта.

---
+++ F_Sharp_1/Program.fs
@@ -0,0 +1,16 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let factorial x : bigint =
+    match x with 
+        | 0 -> 1I
+        | _ -> [1..x] |> Seq.fold (fun acc elem -> acc*bigint elem) 1I
+
+
+[<EntryPoint>]
+let main argv =
+    let x = Console.Read()
+    Console.WriteLine(factorial x)
+    Console.ReadKey()
+    0 // return an integer exit code
