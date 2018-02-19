8c57138c532469b47b550fe89e74896d83053497
MaxVortman <maka.abort@gmail.com>
2018-02-19 14:40:02 +0300

    Creared fibonacci func.

--- F_Sharp_1/Program.fs
+++ F_Sharp_1/Program.fs
@@ -1,16 +1,26 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Numerics
 
 let factorial x : bigint =
     match x with 
         | 0 -> 1I
         | _ -> [1..x] |> Seq.fold (fun acc elem -> acc*bigint elem) 1I
 
+let fibSeq = Seq.unfold (fun state ->
+        Some(fst state + snd state, (snd state, fst state + snd state))) (0I,1I)
+
+let fibonacci x : bigint = 
+    match x with 
+        | 0 -> 0I
+        | 1 -> 1I
+        | _ -> fibSeq |> Seq.take (x - 1) |> Seq.last
+
 
 [<EntryPoint>]
 let main argv =
-    let x = Console.Read()
-    Console.WriteLine(factorial x)
+    let x = Convert.ToInt32(Console.ReadLine())
+    Console.WriteLine("Factorial : {0} \nFibonacci : {1}", factorial x, fibonacci x) 
     Console.ReadKey()
     0 // return an integer exit code
