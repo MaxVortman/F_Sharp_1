3d6a003c2891546c32656a2485621a9eb9c50377
MaxVortman <maka.abort@gmail.com>
2018-02-20 22:09:40 +0300

    Solved, but I think this is don't O(nlogn)

--- F_Sharp_2/Program.fs
+++ F_Sharp_2/Program.fs
@@ -29,9 +29,39 @@ let isPalindrome str =
                                       else checkForEquals (t1, t2) (i + 1) n
     checkForEquals (str, List.rev str) 0 ((List.length str) / 2)
 
+let separateListByTwo n list = 
+    let rec separate (left, right) i = 
+        if i = n then (left, right)
+        else 
+            match (left, right) with
+            | (x, h2 :: t2) -> separate ((h2 :: (List.rev x)) |> List.rev, t2) (i + 1)
+    separate ([], list) 0
+
+
+let rec mergesort list = 
+
+    let merge left right = 
+        let rec mergerec left right res =
+            match (left, right) with
+            | ([], []) -> res
+            | (_, []) -> res @ left
+            | ([], _) -> res @ right
+            | ([a], [b]) -> if a <= b then res @ [a] @ [b] else res @ [b] @ [a]
+            | (h1 :: t1, h2 :: t2) -> if h1 <= h2 then mergerec t1 right (res @ [h1]) 
+                                      else mergerec left t2 (res @ [h2])
+        mergerec left right []
+
+    let length = List.length list
+    if length <= 1 then list
+    else 
+        let left, right = separateListByTwo (length / 2) list
+        merge (mergesort left) (mergesort right)
+
+
+
 [<EntryPoint>]
 let main argv =
-    printfn "Choose the problem:\n1 - mul digits in number\n2 - index of\n3 - palindrome\n"
+    printfn "Choose the problem:\n1 - mul digits in number\n2 - index of\n3 - palindrome\n4 - mergesort"
     let problemNumber = Convert.ToInt32(Console.ReadLine())
     if problemNumber = 1 then 
         printfn "Enter a number: "
@@ -47,6 +77,10 @@ let main argv =
         printfn "Enter the string: "
         let str = Console.ReadLine()
         printfn "%O" (Seq.toList str |> isPalindrome)
+    elif problemNumber = 4 then
+        printfn "Enter the list elem: "
+        let list = Console.ReadLine().Split() |> List.ofArray |> List.map (fun x -> int x)
+        printfn "sorted list: %A" <| mergesort list
     Console.ReadKey() |> ignore
 
     0 // return an integer exit code
