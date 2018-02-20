ba53e3bf4fb6adb335bc7fb31dae542cf1222cab
MaxVortman <maka.abort@gmail.com>
2018-02-20 20:36:10 +0300

    Solved polindrome problem.

--- F_Sharp_2/Program.fs
+++ F_Sharp_2/Program.fs
@@ -17,9 +17,21 @@ let indexOf x list =
                     else index (i + 1) t
     index 0 list
 
+let isPalindrome str = 
+    let rec checkForEquals (list1, list2) i n = 
+        if i = n then true
+        else
+            match (list1, list2) with
+            | ([], []) -> true
+            | ([a], [b]) when a = b -> true
+            | ([a], [b]) -> false
+            | (h1 :: t1, h2 :: t2) -> if h1 <> h2 then false
+                                      else checkForEquals (t1, t2) (i + 1) n
+    checkForEquals (str, List.rev str) 0 ((List.length str) / 2)
+
 [<EntryPoint>]
 let main argv =
-    printfn "Choose the problem:\n1 - mul digits in number\n2 - index of\n"
+    printfn "Choose the problem:\n1 - mul digits in number\n2 - index of\n3 - palindrome\n"
     let problemNumber = Convert.ToInt32(Console.ReadLine())
     if problemNumber = 1 then 
         printfn "Enter a number: "
@@ -31,7 +43,10 @@ let main argv =
         printfn "Enter the elem to find: "
         let x = Convert.ToInt32(Console.ReadLine())
         printfn "Index of %i in list : %i" <| x <| indexOf x list
-
+    elif problemNumber = 3 then
+        printfn "Enter the string: "
+        let str = Console.ReadLine()
+        printfn "%O" (Seq.toList str |> isPalindrome)
     Console.ReadKey() |> ignore
 
     0 // return an integer exit code
