4242cd790a044a346d096219f99943856fddacb2
MaxVortman <maka.abort@gmail.com>
2018-02-27 17:55:39 +0300

    First way added.

---
+++ F_Sharp_3/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open NUnit.Framework
+open FsUnit
+
+module F_Sharp_3_1 =
+
+    let countEvenNumbers list = 
+        let subListLength list = 
+            List.length list - (List.fold (+) 0 list)
+        list |> List.map (fun x -> x % 2) |> subListLength
+
+    [<Test>]
+    let ``Count even numbers of list [0 .. 10] should be equal 6`` =
+        [0 .. 10] |> countEvenNumbers |> should equal 6
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
