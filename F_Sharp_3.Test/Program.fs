541683d0963fca391691e78ac990daa062699e74
MaxVortman <maka.abort@gmail.com>
2018-03-28 12:56:27 +0300

    Now it works.

--- F_Sharp_3.Test/Program.fs
+++ F_Sharp_3.Test/Program.fs
@@ -1,23 +1,18 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿
+module ``3_1Test`` 
 
 open System
 open NUnit.Framework
 open FsUnit
 
-module ``3_1Test`` =
+let ``Count even numbers of list [0 .. 10] should be equal 6`` countEvenNumbers =
+    [0 .. 10] |> countEvenNumbers |> should equal 6
 
-    let ``Count even numbers of list [0 .. 10] should be equal 6`` countEvenNumbers =
-        [0 .. 10] |> countEvenNumbers |> should equal 6
+[<Test>]
+let ``for first option``() = ``Count even numbers of list [0 .. 10] should be equal 6`` F_Sharp_3_1.FirstOption.countEvenNumbers
 
-    [<Test>]
-    let ``for first option`` = ``Count even numbers of list [0 .. 10] should be equal 6`` Program.F_Sharp_3_1.FirstOption.countEvenNumbers
+[<Test>]
+let ``for second option``() = ``Count even numbers of list [0 .. 10] should be equal 6`` F_Sharp_3_1.SecondOption.countEvenNumbers
 
-    [<Test>]
-    let ``for second option`` = ``Count even numbers of list [0 .. 10] should be equal 6`` Program.F_Sharp_3_1.SecondOption.countEvenNumbers
-
-    [<Test>]
-    let ``for third option`` = ``Count even numbers of list [0 .. 10] should be equal 6`` Program.F_Sharp_3_1.ThirdOption.countEvenNumbers
-
-
-[<EntryPoint>]
-let main argv = 0 // return an integer exit code
+[<Test>]
+let ``for third option``() = ``Count even numbers of list [0 .. 10] should be equal 6`` F_Sharp_3_1.ThirdOption.countEvenNumbers
--- F_Sharp_3/Program.fs
+++ F_Sharp_3/Program.fs
@@ -1,8 +1,6 @@
 ﻿// Learn more about F# at http://fsharp.org
 
-open System
-
-module F_Sharp_3_1 =
+module F_Sharp_3_1
 
     module FirstOption = 
         let countEvenNumbers list =            
@@ -15,9 +13,3 @@ module F_Sharp_3_1 =
     module ThirdOption = 
         let countEvenNumbers list = 
             list |> List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0
-
-       
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
