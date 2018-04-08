8c140cc7186dd2c76f52479a4c7ed90521824fbb
MaxVortman <maka.abort@gmail.com>
2018-04-08 16:55:06 +0300

    ...

---
+++ F_Sharp_4/Library.fs
@@ -0,0 +1,10 @@
+namespace F_Sharp_4
+
+//type LambdaExpression =
+//| Term of LambdaExpression * LambdaExpression 
+//| Empty
+
+//let betaReduction expression = 
+//    let rec traversal expression = 
+//        match traversal with 
+//        | Term()
