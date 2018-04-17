fa42ad5a745df194ffad75861113a899efc3c35a
MaxVortman <maka.abort@gmail.com>
2018-04-17 14:14:46 +0300

    I really don't know

--- F_Sharp_4/Library.fs
+++ F_Sharp_4/Library.fs
@@ -1,10 +1,15 @@
 namespace F_Sharp_4
 
-//type LambdaExpression =
-//| Term of LambdaExpression * LambdaExpression 
-//| Empty
+type Term =
+|Norm
+|Redex of char * Term * Term
 
-//let betaReduction expression = 
-//    let rec traversal expression = 
-//        match traversal with 
-//        | Term()
+let normalize t =
+    let rec pods ch t1 t2 = 
+    match t1 with
+    let rec norm t = 
+    match t with
+    |Redex(ch, t1, t2) -> norm <| pods ch t1 t2
+    |Norm -> t
+    
+let test = Redex('f', Redex('x', f))
