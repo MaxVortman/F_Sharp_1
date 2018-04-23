21894aa27bea046f86523333ad27c4cdb9452203
MaxVortman <maka.abort@gmail.com>
2018-04-23 20:15:10 +0300

    more?

--- F_Sharp_5/Program.fs
+++ F_Sharp_5/Program.fs
@@ -1,2 +1,18 @@
 ﻿namespace Generic_Tasks
 
+namespace Generic_Tasks
+
+module ``2`` = 
+
+//func x l = List.map (fun y -> y * x) l
+
+    let func x l = List.map (fun y -> y * x) l
+
+    let func'1 x = List.map (fun y -> y * x)
+
+    let func'2 x = List.map (fun y -> (*) y x)
+
+    let func'3 x = List.map ((*) x)
+
+    //сan I have more?
+
