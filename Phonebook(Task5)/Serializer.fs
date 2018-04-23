06567ad08eb3f74fa7dd1a271d55cd727a90e195
MaxVortman <maka.abort@gmail.com>
2018-04-23 21:14:28 +0300

    Serializer added.

---
+++ Phonebook(Task5)/Serializer.fs
@@ -0,0 +1,23 @@
+﻿namespace Phonebook
+
+open System
+open System.IO
+open System.Runtime.Serialization.Formatters.Binary
+
+
+type Serializer() = 
+
+    let createStream path (mode : FileMode) = new FileStream(path, mode)
+    let formatter = new BinaryFormatter()
+
+    member s.Serialize path (obj : 'a) =         
+        let stream = createStream path FileMode.Create
+        formatter.Serialize(stream, box obj)
+        stream.Close()
+
+    member s.Deserialize path =
+        let stream = createStream path FileMode.Open
+        let res = formatter.Deserialize(stream)
+        stream.Close()
+        unbox res
+
