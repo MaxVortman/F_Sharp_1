10b0cd7858fe79723a5811558a323d0d5c0df395
MaxVortman <maka.abort@gmail.com>
2018-04-23 18:44:02 +0300

    More tests and fixes.

--- F_Sharp_5.Test/Library.fs
+++ F_Sharp_5.Test/Library.fs
@@ -4,7 +4,36 @@ open NUnit.Framework
 open FsUnit
 
 module ``1`` =
+    open NUnit.Framework.Constraints
     
     [<Test>]
     let ``should be correct at sample : "(2 + 5)"`` () = 
         "(2 + 5)" |> Generic_Tasks.``1``.isCorrectRoundBrSeq |> should equal true
+
+    [<Test>]
+    let ``should be correct at sample : "[2 + 5]"`` () = 
+        "[2 + 5]" |> Generic_Tasks.``1``.isCorrectSquareBrSeq |> should equal true
+
+    [<Test>]
+    let ``should be correct at sample : "{2 + 5}"`` () = 
+        "{2 + 5}" |> Generic_Tasks.``1``.isCorrectBracesSeq |> should equal true
+
+    [<Test>]
+    let ``should be true (empty string)`` () = 
+        "" |> Generic_Tasks.``1``.isCorrectBracesSeq |> should equal true
+
+    [<Test>]
+    let ``should be false (one bracket)`` () = 
+        "(" |> Generic_Tasks.``1``.isCorrectRoundBrSeq |> should equal false
+
+    [<Test>]
+    let ``should be correct at sample (braces) : "{FloatingPointNumerics.ReinterpretAsDouble+ds(2 + 5)}]dsdf]ytr][["`` () = 
+        "{FloatingPointNumerics.ReinterpretAsDouble+ds(2 + 5)}]dsdf]ytr][[" |> Generic_Tasks.``1``.isCorrectBracesSeq |> should equal true
+
+    [<Test>]
+    let ``should be correct at sample (round) : "{FloatingPointNumerics.ReinterpretAsDouble+ds(2 + 5)}]dsdf]ytr][["`` () = 
+        "{FloatingPointNumerics.ReinterpretAsDouble+ds(2 + 5)}]dsdf]ytr][[" |> Generic_Tasks.``1``.isCorrectRoundBrSeq |> should equal true
+
+    [<Test>]
+    let ``should be incorrect at sample (square) : "{FloatingPointNumerics.ReinterpretAsDouble+ds(2 + 5)}]dsdf]ytr][["`` () = 
+        "{FloatingPointNumerics.ReinterpretAsDouble+ds(2 + 5)}]dsdf]ytr][[" |> Generic_Tasks.``1``.isCorrectSquareBrSeq |> should equal false
--- F_Sharp_5/Program.fs
+++ F_Sharp_5/Program.fs
@@ -10,7 +10,10 @@ module ``1`` =
 
     let isCorrectBrSeq (str : string) (br : Brackets) = 
         let len = str.Length - 1
-        let rec count acc ch i = 
+        let rec count acc ch i =
+            //must help with samples kinda "] 5 + 1 ["
+            if acc < 0 then acc
+            else
             match ch with
             | ch when i = len -> if ch = br.Open then (acc + 1)
                                  elif ch = br.Close then (acc - 1)
@@ -18,6 +21,7 @@ module ``1`` =
             | ch when ch = br.Open -> count (acc + 1) str.[i + 1] (i + 1)
             | ch when ch = br.Close -> count (acc - 1) str.[i + 1] (i + 1)            
             | _ -> count acc str.[i + 1] (i + 1)
+        if len < 0 then true else
         let acc = count 0 str.[0] 0
         if acc = 0 then true
         else false
