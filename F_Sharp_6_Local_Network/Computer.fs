eb339b4181c635c838e13c103179b11634089aec
MaxVortman <maka.abort@gmail.com>
2018-05-17 01:15:28 +0300

    Something done

--- F_Sharp_6_Local_Network/Computer.fs
+++ F_Sharp_6_Local_Network/Computer.fs
@@ -1,4 +1,15 @@
 ﻿namespace LocalNetwork
 
-type Computer() = 
-
+type Computer(operatingSystem : OperatingSystem) = 
+    let defaultVirus = Virus("None", "No one", "Not working virus", LevelEnum.None)
+    let mutable virus = defaultVirus
+    member this.Infect (?virus : Virus) = 
+        let infectionProbability = (this.OperatingSystem.HoleLevel + defaultArg virus.TruePower - this.OperatingSystem.Antivirus.SecureLevel) % 100
+        match infectionProbability  with
+        | 100 ->    
+                    true
+        |
+    member val OperatingSystem = operatingSystem with get
+    member val IsInfected : bool = false with get
+    member val Virus = 
+    
--- F_Sharp_6_Local_Network/OperatingSystem.fs
+++ F_Sharp_6_Local_Network/OperatingSystem.fs
@@ -12,7 +12,8 @@ type OperatingSystem(name : string, description : string, author : string, holeL
     member val Description = description with get
     member val Author = author with get
     member val HoleLevel = holeLevel with get
+    member val Antivirus = antivirus with get
 
-    member this.Antivirus ?newAntivirus = 
+    member this.InstallAntivirus ?newAntivirus = 
         antivirus = defaultArg newAntivirus defaultAntivirus
     override this.ToString() = String.Format("{0}\nby {1}\n{2}\Insecurity {3}%", this.Name, this.Author, this.Description, this.HoleLevel)
