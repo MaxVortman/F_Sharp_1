796c86ac5d9db9e21726ec62207beab3bf810455
MaxVortman <maka.abort@gmail.com>
2018-05-30 13:43:21 +0300

    Infect procedure implemented.

--- F_Sharp_6_Local_Network/Computer.fs
+++ F_Sharp_6_Local_Network/Computer.fs
@@ -2,14 +2,16 @@
 
 type Computer(operatingSystem : OperatingSystem) = 
     let defaultVirus = Virus("None", "No one", "Not working virus", LevelEnum.None)
-    let mutable virus = defaultVirus
-    member this.Infect (?virus : Virus) = 
-        let infectionProbability = (this.OperatingSystem.HoleLevel + defaultArg virus.TruePower - this.OperatingSystem.Antivirus.SecureLevel) % 100
-        match infectionProbability  with
-        | 100 ->    
-                    true
-        |
+    let mutable currentVirus = defaultVirus
+    let mutable isInfected = false
+    let setVirus virus =    currentVirus <- virus
+                            isInfected <- true 
+    member val Id : int = -1 with get, set
     member val OperatingSystem = operatingSystem with get
     member val IsInfected : bool = false with get
-    member val Virus = 
-    
+    member val Virus = currentVirus with get
+    member this.Infect (virus : Virus) = 
+        let infectionProbability =  int (this.OperatingSystem.HoleLevel + virus.TruePower - this.OperatingSystem.Antivirus.SecureLevel) % 100        
+        let rnd_value = let rnd = new System.Random()
+                        rnd.Next(1, 100)
+        if rnd_value <= infectionProbability then setVirus virus
