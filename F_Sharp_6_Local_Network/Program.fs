d2bbf103c62a157274328c1afaf05d3ad462ec6d
MaxVortman <maka.abort@gmail.com>
2018-05-30 14:33:09 +0300

    Read adjucency matrix implemented.

--- F_Sharp_6_Local_Network/Program.fs
+++ F_Sharp_6_Local_Network/Program.fs
@@ -1,6 +1,17 @@
 ﻿module Start
 
+open System
+
+let readMatrix n =     
+    printfn "Now enter a %ix%i adjucency matrix" n n
+    let rec readLine i acc = 
+        match i with
+        | 0 -> acc
+        | i ->  let splitedLine = Console.ReadLine().Split()
+                readLine (i - 1) (splitedLine :: acc)
+    readLine n []
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    
     0 // return an integer exit code
