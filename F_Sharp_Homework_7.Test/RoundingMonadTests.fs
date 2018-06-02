b93c00236e790168fde8364962a26cf6b6de68cb
MaxVortman <maka.abort@gmail.com>
2018-06-03 00:27:34 +0300

    simple test

---
+++ F_Sharp_Homework_7.Test/RoundingMonadTests.fs
@@ -0,0 +1,15 @@
+﻿namespace F_Sharp_Homework_7.Test
+
+module RoundingMonadTests =
+    open NUnit.Framework
+    open FsUnit
+    open F_Sharp_Homework_7.RoundingMonad
+    
+    [<Test>]
+    let ``should round to 3 digits (0.048)``() = 
+        let res = rounding 3 {
+            let! a = 2.0 / 12.0
+            let! b = 3.5
+            return a / b
+        }
+        res |> should equal 0.048
