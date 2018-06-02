22f31bf61320591b2fb50f238e694b8066ac3cd7
MaxVortman <maka.abort@gmail.com>
2018-06-03 00:40:26 +0300

    7.2 implemented

---
+++ F_Sharp_Homework_7/StringAddMonad.fs
@@ -0,0 +1,16 @@
+﻿namespace F_Sharp_Homework_7
+module StringAddMonad =
+    
+    open System
+
+    type StringAddBuilder() =
+        member this.Bind(x, f) =
+            let mutable z = 0
+            match Int32.TryParse(x, ref z) with
+            | false -> None
+            | true -> f z
+        member this.Return(x) = 
+            Some x
+
+    let calculate = new StringAddBuilder()
+
