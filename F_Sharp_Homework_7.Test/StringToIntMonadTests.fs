016bdf58e29d78dc0a203cf9f2c54b3e4f9c86a3
MaxVortman <maka.abort@gmail.com>
2018-06-03 00:56:28 +0300

    Fix and tested on simple test.

---
+++ F_Sharp_Homework_7.Test/StringToIntMonadTests.fs
@@ -0,0 +1,26 @@
+﻿namespace F_Sharp_Homework_7.Test
+
+module StringToIntMonadTests =
+    open NUnit.Framework
+    open FsUnit
+    open F_Sharp_Homework_7.StringToIntMonad
+    
+    [<Test>]
+    let ``should add 1 to 2`` () =
+        let result = calculate {
+            let! x = "1"
+            let! y = "2"
+            let z = x + y
+            return z
+        }
+        result |> should equal (Some(3))
+
+    [<Test>]
+    let ``should return None`` () =
+        let res = calculate {
+            let! x = "1"
+            let! y = "Ъ"
+            let z = x + y
+            return z
+        }
+        res |> should equal (None)
