90dd1ef70f752550187f6953d363e2145706c761
MaxVortman <maka.abort@gmail.com>
2018-06-04 03:34:24 +0300

    Fixed and tested.

--- F_Sharp_8/Library1.fs
+++ F_Sharp_8/Library1.fs
@@ -5,7 +5,7 @@ open System.Text.RegularExpressions
 open System.IO
 
 let printPageStatAsync url =
-    let urlRegex = new Regex("<a href=\"(https?:\/\/[\w|\W]+?)\">", RegexOptions.Compiled)
+    let urlRegex = new Regex("<a href=\"(https?:\/\/[\w|\W]+?)\"", RegexOptions.Compiled)
     let parseHtml html = 
         urlRegex.Matches(html) |> Seq.cast<Match> |> Seq.map (fun m -> m.Groups.[1].Value)
     async {
@@ -18,8 +18,12 @@ let printPageStatAsync url =
             return (url, html)
             }
         let printStatFor (url, html : string) = 
-            printfn "%s --- %i" url html.Length
-        let! html, _ = getHtml url
-        printStatFor (html, url)
-        parseHtml html |> Seq.map getHtml |> Async.Parallel |> Async.RunSynchronously |> Seq.map printStatFor |> ignore
+            let stat = sprintf "%s --- %i" url html.Length
+            printfn "%s" stat
+            stat
+        let! _, html = getHtml url
+        let seqOfStat = seq{    
+            yield printStatFor (url, html)
+            yield! parseHtml html |> Seq.map getHtml |> Async.Parallel |> Async.RunSynchronously |> Seq.map printStatFor }        
+        return seqOfStat
     }
