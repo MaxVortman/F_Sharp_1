d1e17164ca6e6f031b91d56386d27ad8d627be0c
MaxVortman <maka.abort@gmail.com>
2018-06-05 12:11:57 +0300

    Improved second way.

--- F_Sharp_9/Library1.fs
+++ F_Sharp_9/Library1.fs
@@ -6,35 +6,34 @@ open System.Threading
         abstract member Get: unit -> 'a
 
     type LazyFactory () = 
-        static let mutable singleThreadedInstance = None
-        [<VolatileField>]
-        static let mutable multiThreadedInstance = None
-        static let mutable multiThreadedLockFreeInstance = None
+        static let mutable instance = None
 
         static member CreateSingleThreadedLazy           supplier =
-            match singleThreadedInstance with
-            | None ->       let instance = supplier ()
-                            singleThreadedInstance <- Some(instance)
-                            instance
+            match instance with
+            | None ->       let newInstance = supplier ()
+                            instance <- Some(newInstance)
+                            newInstance
             | Some(v) ->    v
             
         
         static member CreateMultiThreadedLazy            supplier =
-            match Volatile.Read(ref multiThreadedInstance) with
-            | None ->       let instance = supplier ()
-                            Volatile.Write(ref multiThreadedInstance, Some(instance))
-                            instance
+            
+            match instance with
+            | None ->       lock instance (fun() -> 
+                                                    let newInstance = supplier ()
+                                                    instance <- Some(newInstance)
+                                                    newInstance)
             | Some(v) ->    v
                 
         static member CreateLockFreeMultiThreadedLazy    supplier =
             let mutable initialValue = None
             let rec CAS () =               
                 let computedValue = supplier ()
-                initialValue <- multiThreadedLockFreeInstance
-                match Interlocked.CompareExchange(ref multiThreadedLockFreeInstance, Some(computedValue), initialValue) = initialValue with
+                initialValue <- instance
+                match Interlocked.CompareExchange(ref instance, Some(computedValue), initialValue) = initialValue with
                 | true ->   computedValue
                 | false ->  CAS()
-            match multiThreadedLockFreeInstance with
+            match instance with
             | None -> CAS()
             | Some(v) -> v
                     
