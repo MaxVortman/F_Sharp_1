090b94dffc2a53d181add4c99471fa7ae7e294c4
MaxVortman <maka.abort@gmail.com>
2018-08-27 19:18:08 +0300

    Something do

--- F_Sharp_9.Test/Library1.fs
+++ F_Sharp_9.Test/Library1.fs
@@ -6,43 +6,31 @@ open F_Sharp_9
 
 module SingleThreadedTest =
 
-    type TestLazy() =
-        interface ILazy<int> with 
-            member this.Get() = LazyFactory.CreateSingleThreadedLazy (fun _ -> 10)
-
     [<Test>]
     let ``should test on single thread``() =
-        let testLazy = new TestLazy() 
-        (testLazy :> ILazy<int>).Get() |> should equal 10
+        let testLazy = LazyFactory.CreateSingleThreadedLazy (fun _ -> 10) 
+        testLazy.Get() |> should equal 10
 
 module MultiThreadedTest =
     open System.Threading
 
-    type TestLazy() =
-        interface ILazy<int> with 
-            member this.Get() = LazyFactory.CreateMultiThreadedLazy (fun _ -> 10)
-
     [<Test>]
     let ``should test on multi thread``() =
-        let testLazy = new TestLazy() 
+        let testLazy = LazyFactory.CreateMultiThreadedLazy (fun _ -> 10) 
         for i in 1..10 do
-            let t = Thread(ThreadStart(fun _ -> (testLazy :> ILazy<int>).Get() |> ignore))
+            let t = Thread(ThreadStart(fun _ -> testLazy.Get() |> ignore))
             t.Start()
-        (testLazy :> ILazy<int>).Get() |> should equal 10
+        testLazy.Get() |> should equal 10
 
 module MultiThreadedLockFreeTest =
     open System.Threading
 
-    type TestLazy() =
-        interface ILazy<int> with 
-            member this.Get() = LazyFactory.CreateLockFreeMultiThreadedLazy (fun _ -> 10)
-
     [<Test>]
     let ``should test on free lock multi thread``() =
-        let testLazy = new TestLazy() 
+        let testLazy = LazyFactory.CreateLockFreeMultiThreadedLazy (fun _ -> 10)  
         for i in 1..100 do
-            let t = Thread(ThreadStart(fun _ ->     (testLazy :> ILazy<int>).Get() |> ignore))
+            let t = Thread(ThreadStart(fun _ ->     testLazy.Get() |> ignore))
             t.Start()
             Thread.Sleep(100)
-        (testLazy :> ILazy<int>).Get() |> should equal 10
-        (testLazy :> ILazy<int>).Get() |> should equal 10
+        testLazy.Get() |> should equal 10
+        testLazy.Get() |> should equal 10
--- F_Sharp_9/Library1.fs
+++ F_Sharp_9/Library1.fs
@@ -2,38 +2,63 @@
 
 open System.Threading
 open System
-
+    
+    /// <summary>
+    /// Интерфейс, предоставляющий ленивое вычисление
+    /// </summary>
     type ILazy<'a> =
         abstract member Get: unit -> 'a
 
+    /// <summary>
+    /// Фабрика, предоставляющая реализации интерфейса ILazy
+    /// </summary>
     type LazyFactory<'a when 'a : equality> () = 
         static let mutable instance : 'a option = None
         static let lockObj = new Object()
+        /// <summary>
+        /// Предоставляет однопоточную реализацию интрейфейса ILazy
+        /// </summary>
+        /// <param name="supplier"></param>
         static member CreateSingleThreadedLazy           supplier =
-            match instance with
-            | None ->       let newInstance = supplier ()
-                            instance <- Some(newInstance)
-                            newInstance
-            | Some(v) ->    v
+            { new ILazy<'a> with
+                        member this.Get() = 
+                            match instance with
+                            | None ->       let newInstance = supplier ()
+                                            instance <- Some(newInstance)
+                                            newInstance
+                            | Some(v) ->    v
+                                                }
             
         
+        /// <summary>
+        /// Предоставляет многопоточную реализацию интрейфейса ILazy
+        /// </summary>
+        /// <param name="supplier"></param>
         static member CreateMultiThreadedLazy            supplier =
-            
-            match instance with
-            | None ->       lock lockObj (fun() -> 
-                                                    let newInstance = supplier ()
-                                                    instance <- Some(newInstance)
-                                                    newInstance)
-            | Some(v) ->    v
-                
+            { new ILazy<'a> with
+                        member this.Get() =         
+                            match instance with
+                            | None ->       lock lockObj (fun() -> 
+                                                                    let newInstance = supplier ()
+                                                                    instance <- Some(newInstance)
+                                                                    newInstance)
+                            | Some(v) ->    v
+                                                }
+        /// <summary>
+        /// Предоставляет free lock многопоточную реализацию интрейфейса ILazy
+        /// </summary>
+        /// <param name="supplier"></param>
         static member CreateLockFreeMultiThreadedLazy    supplier =
-            let rec CAS () =
-                let currentValue = instance
-                let computedValue = supplier ()
-                match Interlocked.CompareExchange(&instance, Some(computedValue), currentValue) = currentValue with
-                | true ->   computedValue
-                | false ->  Thread.SpinWait 10
-                            CAS()
-            match instance with
-            | None ->       CAS()
-            | Some(v) ->    v
+            { new ILazy<'a> with
+                        member this.Get() =
+                            let rec CAS () =
+                                let currentValue = instance
+                                let computedValue = supplier ()
+                                match Interlocked.CompareExchange(&instance, Some(computedValue), currentValue) = currentValue with
+                                | true ->   computedValue
+                                | false ->  Thread.SpinWait 10
+                                            CAS()
+                            match instance with
+                            | None ->       CAS()
+                            | Some(v) ->    v
+                                                }
