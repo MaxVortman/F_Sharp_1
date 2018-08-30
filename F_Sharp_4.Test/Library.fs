da28c2187dc6298ca7e0f131896df916d9dbfcb8
MaxVortman <maka.abort@gmail.com>
2018-08-30 08:38:58 +0300

    Impelmented

--- F_Sharp_4.Test/Library.fs
+++ F_Sharp_4.Test/Library.fs
@@ -20,3 +20,13 @@ let ``Should test a insertion (lambda x.lambda y. x y) a``() =
 let ``Should test a normalizing (lambda x.lambda y. x y) a``() =
     let f = ('x' *  ('y' * (Varible('x') ^ Varible('y')))) ^ Varible('a')
     normalize f |> should equal ('y' * (Varible('a') ^ Varible('y')))
+
+[<Test>]
+let ``Should test a normalizing (lambda x.lambda y. x y) a b``() =
+    let f = (('x' *  ('y' * (Varible('x') ^ Varible('y')))) ^ Varible('a')) ^ Varible('b')
+    normalize f |> should equal (Varible('a') ^ Varible('b'))
+
+[<Test>]
+let ``Should test a unnormalizing (lambda x.x x x) (lambda x.x x x)``() =
+    let f = ('x' * (Varible('x') ^ Varible('x') ^ Varible('x'))) ^ ('x' * (Varible('x') ^ Varible('x') ^ Varible('x')))
+    (fun() -> normalize f |> ignore) |> should throw typeof<NormalizedFormNotFound>
--- F_Sharp_4/Library.fs
+++ F_Sharp_4/Library.fs
@@ -21,11 +21,28 @@ let pods (F : Term) (x : Term) =
     |Abstraction(c, r) -> postVar c r
     |_ -> F
 
+let depth (a : Term) =
+    let rec depthInternal acc (f : Term) =
+        match f with
+        |Application(l, r) -> depthInternal (acc + 1) l
+        |Abstraction(c, x) -> depthInternal (acc + 1) x
+        |_ -> acc
+    depthInternal 0 a
+
+exception NormalizedFormNotFound
+
 let normalize (A : Term) =
+    let depthA = depth A
+    let check (a : Term) =
+        let deptha = depth a
+        match deptha with
+        |x when depthA > x -> a
+        |_ -> raise NormalizedFormNotFound 
     let rec normalizeInternal (a : Term) =
         match a with
         |Varible(_) | Abstraction(_) -> a
         |Application(l, r) ->   match l with 
-                                |Abstraction(_) -> pods l r |> normalizeInternal
-                                |_ -> normalizeInternal l ^ r |> normalizeInternal
+                                |Abstraction(_) -> pods l r |> check |> normalizeInternal
+                                |Application(_) -> ((normalizeInternal l) ^ r) |> normalizeInternal
+                                |_ -> l ^ r
     normalizeInternal A
