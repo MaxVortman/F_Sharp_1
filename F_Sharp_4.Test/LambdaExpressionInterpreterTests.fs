7b6ca5834c7b9d6355ac8de4d2cc98adcd582914
MaxVortman <maka.abort@gmail.com>
2018-09-21 13:21:50 +0300

    alfa reduction handled

--- F_Sharp_4.Test/LambdaExpressionInterpreterTests.fs
+++ F_Sharp_4.Test/LambdaExpressionInterpreterTests.fs
@@ -5,30 +5,21 @@ open FsUnit
 open F_Sharp_4
 
 [<Test>]
-let ``Should test a simple insertion lambda a.a b``() =
-    let ch = 'a'
-    let var = Varible('a')
-    let x = Varible('b')
-    substitution ch var x |> should equal (Varible('b'))
-
-[<Test>]
-let ``Should test a insertion (lambda x.lambda y. x y) a``() =
-    let ch = 'x'
-    let var = 'y' * (Varible('x') ^ Varible('y'))
-    let x = Varible('a')
-    substitution ch var x |> should equal ('y' * (Varible('a') ^ Varible('y')))
-
-[<Test>]
 let ``Should test a normalizing (lambda x.lambda y. x y) a``() =
-    let f = ('x' *  ('y' * (Varible('x') ^ Varible('y')))) ^ Varible('a')
-    normalize f |> should equal ('y' * (Varible('a') ^ Varible('y')))
+    let f = ("x" *  ("y" * (Varible("x") ^ Varible("y")))) ^ Varible("a")
+    normalize f |> should equal ("y" * (Varible("a") ^ Varible("y")))
 
 [<Test>]
 let ``Should test a normalizing (lambda x.lambda y. x y) a b``() =
-    let f = (('x' *  ('y' * (Varible('x') ^ Varible('y')))) ^ Varible('a')) ^ Varible('b')
-    normalize f |> should equal (Varible('a') ^ Varible('b'))
+    let f = (("x" *  ("y" * (Varible("x") ^ Varible("y")))) ^ Varible("a")) ^ Varible("b")
+    normalize f |> should equal (Varible("a") ^ Varible("b"))
 
 [<Test>]
 let ``Should test a unnormalizing (lambda x.x x x) (lambda x.x x x)``() =
-    let f = ('x' * (Varible('x') ^ Varible('x') ^ Varible('x'))) ^ ('x' * (Varible('x') ^ Varible('x') ^ Varible('x')))
+    let f = ("x" * (Varible("x") ^ Varible("x") ^ Varible("x"))) ^ ("x" * (Varible("x") ^ Varible("x") ^ Varible("x")))
     (fun() -> normalize f |> ignore) |> should throw typeof<NormalizedFormNotFoundException>
+
+[<Test>]
+let ``Should normalize (lambda x.lambda y.x y) y``() =
+    let f = ("x" *  ("y" * (Varible("x") ^ Varible("y")))) ^ Varible("y")
+    normalize f |> should equal ("y" * (Varible("y0") ^ Varible("y")))
--- F_Sharp_4/LambdaExpressionInterpreter.fs
+++ F_Sharp_4/LambdaExpressionInterpreter.fs
@@ -1,46 +1,87 @@
 module F_Sharp_4
 
 type Term =
-| Varible of char
+| Varible of string
 | Application of Term * Term
-| Abstraction of char * Term
+| Abstraction of string * Term
 
-let (*) (ch : char) (term : Term) = Abstraction(ch, term)
+let (*) (ch : string) (term : Term) = Abstraction(ch, term)
 let (^) (term1 :Term) (term2 : Term) = Application(term1, term2)
-
-let substitution (ch : char) (destinationTerm : Term) (term : Term) = 
-    let postVar (c : char) (f : Term) =
-        let rec postVarInternal (g : Term) =
-            match g with
-            |Varible(h) when h = c -> term
-            |Varible(_) -> g
-            |Application(l, r) -> postVarInternal l ^ postVarInternal r
-            |Abstraction(s, r) -> s * postVarInternal r 
-        postVarInternal f
-    postVar ch destinationTerm
-
-let depth (a : Term) =
-    let rec depthInternal acc (f : Term) =
-        match f with
-        |Application(l, r) -> depthInternal (acc + 1) l
-        |Abstraction(c, x) -> depthInternal (acc + 1) x
-        |_ -> acc
-    depthInternal 0 a
 
 exception NormalizedFormNotFoundException
 
 let normalize (sourceTerm : Term) =
+    let getSetOfVar term =
+        let rec travelsolSourceTerm internalTerm = 
+            seq {
+            match internalTerm with
+            |Varible(ch) -> yield ch
+            |Abstraction(ch, t) ->  yield ch
+                                    yield! travelsolSourceTerm t
+            |Application(l, r) ->   yield! travelsolSourceTerm l
+                                    yield! travelsolSourceTerm r
+            }
+        new Set<string>(travelsolSourceTerm term)
+
+    let depth (a : Term) =
+        let rec depthInternal acc (f : Term) =
+            match f with
+            |Application(l, r) -> depthInternal (acc + 1) l
+            |Abstraction(c, x) -> depthInternal (acc + 1) x
+            |_ -> acc
+        depthInternal 0 a
+
     let depthOfSourceTerm = depth sourceTerm
+
     let check (a : Term) =
         let depthOfCurrentTerm = depth a
         match depthOfCurrentTerm with
         |x when depthOfSourceTerm > x -> a
         |_ -> raise NormalizedFormNotFoundException 
+
+    let substitution (ch : string) (destinationTerm : Term) (term : Term) = 
+        let postVar (c : string) (f : Term) =
+            let rec postVarInternal (g : Term) =
+                match g with
+                |Varible(h) when h = c -> term
+                |Varible(_) -> g
+                |Application(l, r) -> postVarInternal l ^ postVarInternal r
+                |Abstraction(s, r) -> s * postVarInternal r 
+            postVarInternal f
+        postVar ch destinationTerm
+    
+    let alfaReduction (setOfVar : Set<string>) (mainTerm : Term) =
+        let rec alfaReductionTravelsol (term : Term) =
+            match term with
+            |Varible(str) | Abstraction(str, _) when setOfVar.Contains str -> alfaReductionRenaming term str
+            |Varible(_) -> term
+            |Abstraction(str, t) -> str * alfaReductionTravelsol t
+            |Application(l, r) -> alfaReductionTravelsol l ^ alfaReductionTravelsol r
+        and alfaReductionRenaming (term : Term) (varible : string) =
+            let unicName =
+                let rec findUnicName i =
+                    let tryName = varible + (string i)
+                    match tryName with
+                    |_ when setOfVar.Contains tryName -> findUnicName <| i + 1
+                    |_ -> tryName
+                findUnicName 0
+            let afterReductionTerm =
+                let rec afterReductionInternal internalTerm = 
+                    match internalTerm with
+                    |Varible(str) when str = varible -> Varible(unicName)
+                    |Varible(_) -> internalTerm
+                    |Abstraction(str, t) when str = varible -> unicName * afterReductionInternal t
+                    |Abstraction(str, t) -> str * afterReductionInternal t
+                    |Application(l, r) -> afterReductionInternal l ^ afterReductionInternal r
+                afterReductionInternal term
+            alfaReductionTravelsol afterReductionTerm
+        alfaReductionTravelsol mainTerm
+
     let rec normalizeInternal (a : Term) =
         match a with
         |Varible(_) | Abstraction(_) -> a
         |Application(l, r) ->   match l with 
-                                |Abstraction(ch, t) -> substitution ch t r |> check |> normalizeInternal
+                                |Abstraction(ch, t) -> alfaReduction (getSetOfVar l) r |> substitution ch t |> check |> normalizeInternal
                                 |Application(_) -> ((normalizeInternal l) ^ r) |> normalizeInternal
                                 |_ -> l ^ r
     normalizeInternal sourceTerm
