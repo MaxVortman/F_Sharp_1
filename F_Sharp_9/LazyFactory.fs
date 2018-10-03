221e806d4dca1453634d6fc1bbf4466d3f00e06b
MaxVortman <maka.abort@gmail.com>
2018-10-03 14:24:21 +0300

    Fix spaces

--- F_Sharp_9/LazyFactory.fs
+++ F_Sharp_9/LazyFactory.fs
@@ -3,65 +3,65 @@
 open System.Threading
 open System
     
-    /// <summary>
-    /// Интерфейс, предоставляющий ленивое вычисление
-    /// </summary>
-    type ILazy<'a> =
-        abstract member Get: unit -> 'a
+/// <summary>
+/// Интерфейс, предоставляющий ленивое вычисление
+/// </summary>
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
 
+/// <summary>
+/// Фабрика, предоставляющая реализации интерфейса ILazy
+/// </summary>
+type LazyFactory<'a when 'a : equality> () = 
+    [<VolatileField>]
+    static let mutable instance : 'a option = None
+    static let lockObj = new Object()
     /// <summary>
-    /// Фабрика, предоставляющая реализации интерфейса ILazy
+    /// Предоставляет однопоточную реализацию интрейфейса ILazy
     /// </summary>
-    type LazyFactory<'a when 'a : equality> () = 
-        [<VolatileField>]
-        static let mutable instance : 'a option = None
-        static let lockObj = new Object()
-        /// <summary>
-        /// Предоставляет однопоточную реализацию интрейфейса ILazy
-        /// </summary>
-        /// <param name="supplier">Функция, вычисляющая значение instance</param>
-        static member CreateSingleThreadedLazy supplier =
-            { new ILazy<'a> with
-                        member this.Get() = 
-                            match instance with
-                            | None ->       let newInstance = supplier ()
-                                            instance <- Some(newInstance)
-                                            newInstance
-                            | Some(v) ->    v
-                                                }
-            
+    /// <param name="supplier">Функция, вычисляющая значение instance</param>
+    static member CreateSingleThreadedLazy supplier =
+        { new ILazy<'a> with
+                    member this.Get() = 
+                        match instance with
+                        | None ->       let newInstance = supplier ()
+                                        instance <- Some(newInstance)
+                                        newInstance
+                        | Some(v) ->    v
+                                            }
         
-        /// <summary>
-        /// Предоставляет многопоточную реализацию интрейфейса ILazy
-        /// </summary>
-        /// <param name="supplier">Функция, вычисляющая значение instance</param>
-        static member CreateMultiThreadedLazy supplier =
-            { new ILazy<'a> with
-                        member this.Get() =         
+    
+    /// <summary>
+    /// Предоставляет многопоточную реализацию интрейфейса ILazy
+    /// </summary>
+    /// <param name="supplier">Функция, вычисляющая значение instance</param>
+    static member CreateMultiThreadedLazy supplier =
+        { new ILazy<'a> with
+                    member this.Get() =         
+                        match instance with
+                        | None ->       lock lockObj (fun() ->  match instance with
+                                                                | None ->       let newInstance = supplier ()
+                                                                                instance <- Some(newInstance)
+                                                                                newInstance
+                                                                | Some(v) ->    v)
+                        | Some(v) ->    v
+                                            }
+    /// <summary>
+    /// Предоставляет lock-free многопоточную реализацию интрейфейса ILazy
+    /// </summary>
+    /// <param name="supplier">Функция, вычисляющая значение instance</param>
+    static member CreateLockFreeMultiThreadedLazy supplier =
+        { new ILazy<'a> with
+                    member this.Get() =
+                        let rec CAS () =
                             match instance with
-                            | None ->       lock lockObj (fun() ->  match instance with
-                                                                    | None ->       let newInstance = supplier ()
-                                                                                    instance <- Some(newInstance)
-                                                                                    newInstance
-                                                                    | Some(v) ->    v)
+                            | None ->       CASInternal ()
                             | Some(v) ->    v
-                                                }
-        /// <summary>
-        /// Предоставляет lock-free многопоточную реализацию интрейфейса ILazy
-        /// </summary>
-        /// <param name="supplier">Функция, вычисляющая значение instance</param>
-        static member CreateLockFreeMultiThreadedLazy supplier =
-            { new ILazy<'a> with
-                        member this.Get() =
-                            let rec CAS () =
-                                match instance with
-                                | None ->       CASInternal ()
-                                | Some(v) ->    v
-                            and CASInternal () =
-                                let currentValue = instance
-                                let computedValue = supplier ()
-                                match Interlocked.CompareExchange(&instance, Some(computedValue), currentValue) = currentValue with
-                                | true ->   computedValue
-                                | false ->  CAS ()
-                            CAS ()
-                                                }
+                        and CASInternal () =
+                            let currentValue = instance
+                            let computedValue = supplier ()
+                            match Interlocked.CompareExchange(&instance, Some(computedValue), currentValue) = currentValue with
+                            | true ->   computedValue
+                            | false ->  CAS ()
+                        CAS ()
+                                            }
